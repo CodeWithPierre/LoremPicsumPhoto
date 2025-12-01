ce40c29f90c1f76b21a1d2e72721f08a3ae43f0a
CodeWithPierre <iryamukurup@gmail.com>
2025-11-30 22:15:12 -0500

    initial project setup

---
+++ LoremPicsumPhoto/LoremPicsumPhoto/LoremPicsumPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoremPicsumPhotoApp.swift
+//  LoremPicsumPhoto
+//
+//  Created by Pierre ILYAMUKURU on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct LoremPicsumPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
